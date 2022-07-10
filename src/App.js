7f606858a34dff9bbad1b6e48ceab5eec7c486b9
Reeyna <reeynarl@gmail.com>
2022-07-10 15:22:24 -0500

    [ADD] styles and buttons component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,52 @@
 import './App.css';
+import logo from './img/logo-freecodecamp.png'
+import Boton from './components/Boton';
+import BotonClear from './components/BotonClear';
+import Pantalla from './components/Pantalla';
+import { useState } from 'react';
 
 function App() {
+  // const [input, setInput] = useState(0)
+  // setInput()
+  // const input = () =>{
+
+  // }
+
   return (
-    <div className="App">
-      
+    <div className='App'>
+      <div className='freecodecamp-logo-contenedor'>
+        <img src={logo} className='freecodecamp-logo' alt='Logo freecodecamp' />
+      </div>
+      <div className='contenedor-calculadora'>
+        <Pantalla input={''} />
+        <div className='fila'>
+          <Boton>1</Boton>
+          <Boton>2</Boton>
+          <Boton>3</Boton>
+          <Boton>+</Boton>
+        </div>
+        <div className='fila'>
+          <Boton>4</Boton>
+          <Boton>5</Boton>
+          <Boton>6</Boton>
+          <Boton>-</Boton>
+        </div>
+        <div className='fila'>
+          <Boton>7</Boton>
+          <Boton>8</Boton>
+          <Boton>9</Boton>
+          <Boton>*</Boton>
+        </div>
+        <div className='fila'>
+          <Boton>=</Boton>
+          <Boton>0</Boton>
+          <Boton>.</Boton>
+          <Boton>/</Boton>
+        </div>
+        <div className='fila'>
+          <BotonClear>Clear</BotonClear>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Boton.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import '../css/Boton.css'
+
+const Boton = (props) => {
+
+  const esOperador = valor =>{
+    return isNaN(valor) && (valor !== '.') && (valor !== '=')
+  }
+
+  return (
+    //Eliminamos (con trimEnd ) el espacio en blanco al final de cuando no se asigna la clase.
+    <div className={`boton-contenedor ${esOperador(props.children) ? 'operador': ''}`.trimEnd()}>
+      {props.children}
+    </div>
+  )
+}
+
+export default Boton
